3f491ae02bbb877d3ea7e82d7dcef493c5c403eb
marlon22021976 <marlon.costa@aluno.fmpsc.edu.br>
2025-04-07 10:22:40 -0300

    Add files via upload

---
+++ Exercicios_ProgI_2025_1/Exercicios_ProgI_2025_1/Exercicio1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 1 - Verificação de Número Positivo, Negativo ou Zero</title>
+</head>
+<body>
+    <h2>Resolução do Exercício 1 <p>- Verificação de Número Positivo, Negativo ou Zero</h2>
+    <?php 
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $numero = $_POST["numero"];
+        
+        
+            if ($numero > 0) {
+                echo "<p>Valor Positivo</p>";
+            } elseif ($numero < 0) {
+                echo "<p>Valor Negativo</p>";
+            } else {
+                echo "<p>Igual a Zero</p>";
+            }
+        }
+        
+    ?>
+
+    <?php
+        echo "<p>";
+        setlocale(LC_ALL, 'pt_BR.utf8');
+        echo strftime('%d/%B/%G');
+    
+        echo "<p>";
+        echo "Informações do browser (User agente):" . $_SERVER
+        ['HTTP_USER_AGENT'];
+    ?>
+</body>
+</html>
